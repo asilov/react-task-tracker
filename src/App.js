9bba2325e294674d4c9c4c028ec853253c3b2329
Asilbek Asilov <asil.california@gmail.com>
2022-11-11 00:17:48 -0800

    Add header and button components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from './components/Tasks'
 
 function App() {
+    const [tasks, setTasks] = useState([
+			{
+				id: 1,
+				text: 'Doctors App',
+				day: 'Feb 5th at 2:30px',
+				reminder: true,
+			},
+			{
+				id: 2,
+				text: 'Go shopping',
+				day: 'March 5th at 2:30px',
+				reminder: true,
+			},
+			{
+				id: 3,
+				text: 'Do homework',
+				day: 'Apr 5th at 2:30px',
+				reminder: true,
+			},
+		])
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
---
+++ src/components/Tasks.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Tasks = () => {
+  
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Tasks
